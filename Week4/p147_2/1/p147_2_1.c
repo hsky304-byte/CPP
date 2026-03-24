c19b3ee0f6795116bdf5fb0bc83226ff3872cd2d
hsky304-byte <hsky304@gmail.com>
2026-03-24 11:49:27 +0900

    Update p147_2_1.c

--- Week4/p147_2/1/p147_2_1.c
+++ Week4/p147_2/1/p147_2_1.c
@@ -1,1 +1,24 @@
-#includ <stdio.h>
+#include <stdio.h>
+
+int reverse(int n)
+{
+    int result = 0;
+
+    while(n != 0)
+    {
+        result = result * 10 + n % 10;
+        n = n / 10;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int num = 12345;
+    int r;
+
+    r = reverse(num);
+
+    printf("reverse=%d", r);
+}
