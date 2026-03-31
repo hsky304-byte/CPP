6dc4c37097816effaa118b636b476eadd3d7164a
hsky304-byte <hsky304@gmail.com>
2026-03-31 13:26:09 +0900

    Update p147_6_1.cpp

--- Week4/p147_6/1/p147_6_1.cpp
+++ Week4/p147_6/1/p147_6_1.cpp
@@ -1,7 +1,24 @@
-01 #include <cstdio>
-02 #include <cstdlib>
-03 #include <ctime>
-04
-05 extern double tElapsed;          // 게임 소요 시간
-06 // nPlay: 문제 수, mode: 1(구구단), 2(두 자리 곱셈), 3(덧셈)
-07 extern double playSpeedGame(int nPlay, int mode); 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// (1) 여러 자리 덧셈 문제를 출제하는 함수
+void playAdd() 
+{
+    // 100~999 사이의 세 자리 수 두 개 생성
+    int a = rand() % 900 + 100; 
+    int b = rand() % 900 + 100;
+    int answer;
+
+    cout << "[덧셈 문제] " << a << " + " << b << " = ? ";
+    cin >> answer;
+
+    // 사용자가 입력한 값과 실제 합을 비교
+    if (answer == a + b) {
+        cout << "정답입니다! 연산 천재시군요." << endl;
+    } else {
+        cout << "오답입니다. 정답은 " << a + b << "입니다." << endl;
+    }
+}
