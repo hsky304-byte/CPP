5930ba20cc7ce751455252ecc6efc4c74fe7941d
hsky304-byte <hsky304@gmail.com>
2026-03-31 13:43:11 +0900

    Update p147_2_5.cpp

--- Week4/p147_2/3/p147_2_5.cpp
+++ Week4/p147_2/3/p147_2_5.cpp
@@ -1,37 +1,14 @@
-int reverse(int n);
-
-
-#include "reverse.h"
+#include "147p_2_5.h"
 
 int reverse(int n)
 {
     int result = 0;
 
-    while(n != 0)
+    while (n > 0)
     {
         result = result * 10 + n % 10;
-        n = n / 10;
+        n /= 10;
     }
 
     return result;
 }
-
-
-#include <stdio.h>
-#include "reverse.h"
-
-int main()
-{
-    int num;
-    int r;
-
-    printf("정수 입력: ");
-    scanf("%d", &num);
-
-    r = reverse(num);
-
-    printf("뒤집은 숫자 = %d\n", r);
-
-    return 0;
-}
-
