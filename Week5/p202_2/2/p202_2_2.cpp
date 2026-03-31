1c1039bf0f5ef4c2001a24e3e0bc0e9616fc8e3c
hsky304-byte <hsky304@gmail.com>
2026-03-31 12:38:44 +0900

    Update p202_2_2.cpp

--- Week5/p202_2/2/p202_2_2.cpp
+++ Week5/p202_2/2/p202_2_2.cpp
@@ -1,32 +1,22 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
-void randomMap(int map[5][5]);
 void printMap(int map[5][5]);
 
 int main()
 {
-    int map[5][5];
+    int map[5][5] =
+    {
+        {1,0,1,0,1},
+        {0,1,0,1,0},
+        {1,0,1,0,1},
+        {0,1,0,1,0},
+        {1,0,1,0,1}
+    };
 
-    srand(time(0));
-
-    randomMap(map);
     printMap(map);
 
     return 0;
-}
-
-void randomMap(int map[5][5])
-{
-    for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<5;j++)
-        {
-            map[i][j] = rand() % 2;
-        }
-    }
 }
 
 void printMap(int map[5][5])
@@ -35,8 +25,8 @@ void printMap(int map[5][5])
     {
         for(int j=0;j<5;j++)
         {
-            cout << map[i][j] << " ";
+            cout<<map[i][j]<<" ";
         }
-        cout << endl;
+        cout<<endl;
     }
 }
