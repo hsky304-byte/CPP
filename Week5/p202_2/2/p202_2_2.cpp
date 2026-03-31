9fa70f6062631eaa62faa05eb6055f54a1ee674a
hsky304-byte <hsky304@gmail.com>
2026-03-31 12:42:39 +0900

    Update p202_2_2.cpp

--- Week5/p202_2/2/p202_2_2.cpp
+++ Week5/p202_2/2/p202_2_2.cpp
@@ -21,12 +21,16 @@ int main()
 
 void printMap(int map[5][5])
 {
+    cout << "   0 1 2 3 4" << endl;   // 제목(열 번호)
+
     for(int i=0;i<5;i++)
     {
+        cout << i << "  ";          // 행 번호 출력
+
         for(int j=0;j<5;j++)
         {
-            cout<<map[i][j]<<" ";
+            cout << map[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
