f0122637aafe34e32251e34c2f59fbfdcf3239b2
hsky304-byte <hsky304@gmail.com>
2026-03-31 15:50:23 +0900

    Update p202_2_1.cpp

--- Week5/p202_2/1/p202_2_1.cpp
+++ Week5/p202_2/1/p202_2_1.cpp
@@ -1,30 +1,23 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void randomMap(int map[5][5]);
-
-int main()
-{
-    int map[5][5];
-
-    srand(time(0));
-
-    randomMap(map);
-
-    return 0;
+// (1) 5x5 배열에 0과 1을 임의로 채우는 함수
+void randomMap(int map[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            map[i][j] = rand() % 2; // 0 또는 1 무작위 생성
+        }
+    }
 }
 
-void randomMap(int map[5][5])
-{
-    for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<5;j++)
-        {
-            map[i][j] = rand()%2;
-            cout<<map[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+int main() {
+    srand(time(NULL)); // 실행할 때마다 다른 값이 나오도록 시드 설정
+    int map[5][5];
+
+    printf("--- 1단계: Random Map 생성 ---\n");
+    randomMap(map);
+    printf("배열 생성이 완료되었습니다.\n");
+
+    return 0;
 }
