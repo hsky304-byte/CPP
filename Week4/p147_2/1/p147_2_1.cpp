f85cbcb706ec4607425a9834425d6b1bdb6f481c
hsky304-byte <hsky304@gmail.com>
2026-03-31 13:30:27 +0900

    Update p147_2_1.cpp

--- Week4/p147_2/1/p147_2_1.cpp
+++ Week4/p147_2/1/p147_2_1.cpp
@@ -1,24 +1,16 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int reverse(int n)
-{
+int reverse(int n) {
     int result = 0;
-
-    while(n != 0)
-    {
-        result = result * 10 + n % 10;
-        n = n / 10;
+    while (n > 0) {
+        result = result * 10 + (n % 10);
+        n /= 10;
     }
-
     return result;
 }
 
-int main()
-{
-    int num = 12345;
-    int r;
-
-    r = reverse(num);
-
-    printf("reverse=%d", r);
+int main() {
+    cout << reverse(12345) << endl;
+    return 0;
 }
